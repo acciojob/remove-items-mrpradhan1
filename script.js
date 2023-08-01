1936d7c08c4361874d8f57b48f52e44d885f41b1
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-08-01 15:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function remove() {
+	colorSelect=document.getElementById("colorSelect");
+	selectedColorIndex=colorSelect.SelectedIndex;
+	if(selectedColorIndex !== -1)
+		colorSelect.removeChild(colorSelect.options[selectedColorIndex]);
+}
+
+const removeButton=document.getElementsByTagName('input')[0];
+removeButton = addEventListener('click', remove);
